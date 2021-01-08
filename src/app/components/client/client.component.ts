007957cd6cc38b4016d686e3894b99d80b7f7153
Ellis Goranov <me@ellis.pw>
2021-01-08 21:43:39 +0000

    change how client gets swfs

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -21,45 +21,46 @@ export class ClientComponent implements OnInit, AfterViewInit {
 	}
 
 	ngAfterViewInit(): void {
-		//this.userService.do_client_auth()
 		this.tokenStorage.getSSO()
 		.subscribe((auth_token: any) => {
-			let swfURL = "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/Habbo.swf";
-			let swfBase = "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/";
-			let flashVarsString = "";
+			let swfURL = environment.HABBO_SWF;
+			let swfBase = environment.PRODUCTION_BASE;
+			
 			let flashVars = {
 				"client.allow.cross.domain": "0",
 				"client.notify.cross.domain": "1",
-				"connection.info.host": "161.97.92.57",
-				"connection.info.port": "30000",
-				"site.url": "http://161.97.92.57",
-				"url.prefix": "http://161.97.92.57",
-				"client.reload.url": "http://161.97.92.57/",
-				"client.fatal.error.url": "http://161.97.92.57",
-				"client.connection.failed.url": "http://161.97.92.57",
-				"external.override.texts.txt": "http://161.97.92.57/assets/swf/gamedata/override/external_flash_override_texts.txt",
-				"external.override.variables.txt": "http://161.97.92.57/assets/swf/gamedata/override/external_override_variables.txt", 	
-				"external.variables.txt": "http://161.97.92.57/assets/swf/gamedata/external_variables.txt",
-				"external.texts.txt": "http://161.97.92.57/assets/swf/gamedata/external_flash_texts.txt",
-				"external.figurepartlist.txt": "http://161.97.92.57/assets/swf/gamedata/figuredata.xml", //PRODUCTION-201701242205-837386173
-				"flash.dynamic.avatar.download.configuration": "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/figuremap.xml",
-				"productdata.load.url": "http://161.97.92.57/assets/swf/gamedata/productdata.txt", 
-				"furnidata.load.url": "http://161.97.92.57/assets/swf/gamedata/furnidata.xml",
+				"connection.info.host": environment.CLIENT_HOST,
+				"connection.info.port": environment.CLIENT_PORT,
+				"site.url": environment.URL,
+				"url.prefix": environment.URL,
+				"client.reload.url": environment.URL + "/disconnected",
+				"client.fatal.error.url": environment.URL + "/disconnected",
+				"client.connection.failed.url": environment.URL + "/disconnected",
+				"external.override.texts.txt": environment.EXTERNAL_OVERRIDE_TEXTS,
+				"external.override.variables.txt": environment.EXTERNAL_OVERRIDE_VARIABLES, 	
+				"external.variables.txt": environment.EXTERNAL_VARIABLES,
+				"external.texts.txt": environment.EXTERNAL_TEXTS,
+				"external.figurepartlist.txt": environment.FIGURE_DATA,
+				"flash.dynamic.avatar.download.configuration": environment.FIGURE_MAP,
+				"productdata.load.url": environment.PRODUCT_DATA, 
+				"furnidata.load.url": environment.FURNI_DATA,
 				"use.sso.ticket": "1",
-				"sso.ticket": auth_token || "",
+				"sso.ticket": auth_token,
 				"processlog.enabled": "0",
-				"client.starting": "uwu is loading...",
-				"flash.client.url": "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/",
+				"client.starting": "Sky is loading...",
+				"flash.client.url": environment.PRODUCTION_BASE,
 				"flash.client.origin": "popup",
 				"ads.domain": "",
 				"diamonds.enabled": '1',
-			  }
+			}
+
+			let flashVarsString = "";
 			for(let [key,value] of Object.entries(flashVars)) {
 				flashVarsString += key + '=' + value + '&amp;';
 			}
-			this.renderer.setProperty(this.elRef.nativeElement, 'innerHTML', `
-			<object id="flash-container" type="application/x-shockwave-flash" data="${swfURL}" width="100%" height="100%"><param name="base" value="${swfBase}"><param name="allowScriptAccess" value="always"><param name="menu" value="false"><param name="wmode" value="opaque"><param name="flashvars" value="${flashVarsString}"></object>
-			`);
+
+			this.renderer.setProperty(this.elRef.nativeElement, 'innerHTML', `<object id="flash-container" type="application/x-shockwave-flash" data="${swfURL}" width="100%" height="100%"><param name="base" value="${swfBase}"><param name="allowScriptAccess" value="always"><param name="menu" value="false"><param name="wmode" value="opaque"><param name="flashvars" value="${flashVarsString}"></object>`);
+			
 			(window as any).HabboFlashClient = {
 				started: !1,
 				init: () =>
@@ -70,6 +71,7 @@ export class ClientComponent implements OnInit, AfterViewInit {
 					}, 1000);
 				}
 			};
+
 			window.addEventListener("load", (window as any).HabboFlashClient.init());
 
 			(window as any).FlashExternalInterface.track = (action: string, label: string, value: string) =>
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,10 +11,22 @@ export const environment = {
   CLIENT_HOST: "161.97.92.57",
   CLIENT_PORT: "30000",
 
+  URL: "http://161.97.92.57",
+
+  HABBO_SWF: "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/Habbo.swf",
+  PRODUCTION_BASE: "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/",
+
   EXTERNAL_VARIABLES: "http://161.97.92.57/assets/swf/gamedata/external_variables.txt",
   EXTERNAL_TEXTS: "http://161.97.92.57/assets/swf/gamedata/external_flash_texts.txt",
+
+  EXTERNAL_OVERRIDE_VARIABLES: "http://161.97.92.57/assets/swf/gamedata/override/external_override_variables.txt",
+  EXTERNAL_OVERRIDE_TEXTS: "http://161.97.92.57/assets/swf/gamedata/override/external_flash_override_texts.txt",
   
-  FIGURE_LIST: "http://161.97.92.57/assets/swf/gamedata/external_flash_texts.txt",
+  FIGURE_DATA: "http://161.97.92.57/assets/swf/gamedata/figuredata.xml",
+  FIGURE_MAP: "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/figuremap.xml",
+
+  PRODUCT_DATA: "http://161.97.92.57/assets/swf/gamedata/productdata.txt",
+  FURNI_DATA: "http://161.97.92.57/assets/swf/gamedata/furnidata.xml",
 };
 
 /*
