5a97980498a5f970d25ce415b7177fe009cfc09d
Ellis Goranov <me@ellis.pw>
2021-01-08 20:55:08 +0000

    add them to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientComponent } from './components/client/client.component';
+import { LeaderboardsComponent } from './components/leaderboards/leaderboards.component';
 
 import { LoginComponent } from "./components/login/login.component";
 import { MeComponent } from './components/me/me.component';
+import { OnlineComponent } from './components/online/online.component';
+import { RegisterComponent } from './components/register/register.component';
 import { StaffComponent } from './components/staff/staff.component';
+import { StoreComponent } from './components/store/store.component';
 import { AuthGuard } from './_helpers/auth.guard';
 import { DashboardComponent } from "./_layouts/dashboard/dashboard.component";
 import { SiteComponent } from "./_layouts/site/site.component";
@@ -16,12 +20,17 @@ const routes: Routes = [
     component: SiteComponent,
     children: [
       { path: "login", component: LoginComponent },
+      { path: "register", component: RegisterComponent }
     ]
   },
   
   { path: "me", component: MeComponent, canActivate: [AuthGuard] },
   { path: "client", component: ClientComponent, canActivate: [AuthGuard] },
   { path: "staff", component: StaffComponent, canActivate: [AuthGuard] },
+
+  { path: "leaderboards", component: LeaderboardsComponent, canActivate: [AuthGuard] },
+  { path: "online", component: OnlineComponent, canActivate: [AuthGuard] },
+  { path: "store", component: StoreComponent, canActivate: [AuthGuard] },
 
   { path: "**", redirectTo: "/login", pathMatch: "full" }
 ];
