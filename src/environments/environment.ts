9b9ffcf577e3f3c3efdff37fe78fb2fb00491271
Ellis Goranov <me@ellis.pw>
2021-01-09 14:58:17 +0000

    change directories

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,25 +8,25 @@ export const environment = {
   API_URL: "/api",
 
 
-  CLIENT_HOST: "161.97.92.57",
+  CLIENT_HOST: "sky-hotel.co.uk",
   CLIENT_PORT: "30000",
 
-  URL: "http://161.97.92.57",
+  URL: "http://sky-hotel.co.uk",
 
-  HABBO_SWF: "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/Habbo.swf",
-  PRODUCTION_BASE: "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/",
+  HABBO_SWF: "http://sky-hotel.co.uk/assets/swf/gordon/PRODUCTION-201701242205-837386173/Habbo.swf",
+  PRODUCTION_BASE: "http://sky-hotel.co.uk/assets/swf/gordon/PRODUCTION-201701242205-837386173/",
 
-  EXTERNAL_VARIABLES: "http://161.97.92.57/assets/swf/gamedata/external_variables.txt",
-  EXTERNAL_TEXTS: "http://161.97.92.57/assets/swf/gamedata/external_flash_texts.txt",
+  EXTERNAL_VARIABLES: "http://sky-hotel.co.uk/assets/swf/gamedata/external_variables.txt",
+  EXTERNAL_TEXTS: "http://sky-hotel.co.uk/assets/swf/gamedata/external_flash_texts.txt",
 
-  EXTERNAL_OVERRIDE_VARIABLES: "http://161.97.92.57/assets/swf/gamedata/override/external_override_variables.txt",
-  EXTERNAL_OVERRIDE_TEXTS: "http://161.97.92.57/assets/swf/gamedata/override/external_flash_override_texts.txt",
+  EXTERNAL_OVERRIDE_VARIABLES: "http://sky-hotel.co.uk/assets/swf/gamedata/override/external_override_variables.txt",
+  EXTERNAL_OVERRIDE_TEXTS: "http://sky-hotel.co.uk/assets/swf/gamedata/override/external_flash_override_texts.txt",
   
-  FIGURE_DATA: "http://161.97.92.57/assets/swf/gamedata/figuredata.xml",
-  FIGURE_MAP: "http://161.97.92.57/assets/swf/gordon/PRODUCTION-201701242205-837386173/figuremap.xml",
+  FIGURE_DATA: "http://sky-hotel.co.uk/assets/swf/gamedata/figuredata.xml",
+  FIGURE_MAP: "http://sky-hotel.co.uk/assets/swf/gordon/PRODUCTION-201701242205-837386173/figuremap.xml",
 
-  PRODUCT_DATA: "http://161.97.92.57/assets/swf/gamedata/productdata.txt",
-  FURNI_DATA: "http://161.97.92.57/assets/swf/gamedata/furnidata.xml",
+  PRODUCT_DATA: "http://sky-hotel.co.uk/assets/swf/gamedata/productdata.txt",
+  FURNI_DATA: "http://sky-hotel.co.uk/assets/swf/gamedata/furnidata.xml",
 };
 
 /*
