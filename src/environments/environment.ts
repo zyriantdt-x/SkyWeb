b94a523f4c998f426ea3082f7bbb71c643cadfa1
Ellis Goranov <me@ellis.pw>
2021-01-09 19:33:58 +0000

    changed some variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,20 +13,20 @@ export const environment = {
 
   URL: "http://sky-hotel.co.uk",
 
-  HABBO_SWF: "http://sky-hotel.co.uk/assets/swf/gordon/PRODUCTION-202006192205-424220153/Habbo.swf",
-  PRODUCTION_BASE: "http://sky-hotel.co.uk/assets/swf/gordon/PRODUCTION-202006192205-424220153/",
+  HABBO_SWF: "http://sky-hotel.co.uk/arc/swf/gordon/PRODUCTION-202006192205-424220153/Habbo.swf",
+  PRODUCTION_BASE: "http://sky-hotel.co.uk/arc/swf/gordon/PRODUCTION-202006192205-424220153/",
 
-  EXTERNAL_VARIABLES: "http://sky-hotel.co.uk/assets/swf/gamedata/external_variables.txt",
-  EXTERNAL_TEXTS: "http://sky-hotel.co.uk/assets/swf/gamedata/external_flash_texts.txt",
+  EXTERNAL_VARIABLES: "http://sky-hotel.co.uk/arc/swf/gamedata/external_variables.txt",
+  EXTERNAL_TEXTS: "http://sky-hotel.co.uk/arc/swf/gamedata/external_flash_texts.txt",
 
-  EXTERNAL_OVERRIDE_VARIABLES: "http://sky-hotel.co.uk/assets/swf/gamedata/override/external_override_variables.txt",
-  EXTERNAL_OVERRIDE_TEXTS: "http://sky-hotel.co.uk/assets/swf/gamedata/override/external_flash_override_texts.txt",
+  EXTERNAL_OVERRIDE_VARIABLES: "http://sky-hotel.co.uk/arc/swf/gamedata/override/external_override_variables.txt",
+  EXTERNAL_OVERRIDE_TEXTS: "http://sky-hotel.co.uk/arc/swf/gamedata/override/external_flash_override_texts.txt",
   
-  FIGURE_DATA: "http://sky-hotel.co.uk/assets/swf/gamedata/figuredata.xml",
-  FIGURE_MAP: "http://sky-hotel.co.uk/assets/swf/gordon/PRODUCTION-202006192205-424220153/figuremap.xml",
+  FIGURE_DATA: "http://sky-hotel.co.uk/arc/swf/gamedata/figuredata.xml",
+  FIGURE_MAP: "http://sky-hotel.co.uk/arc/swf/gordon/PRODUCTION-202006192205-424220153/figuremap.xml",
 
-  PRODUCT_DATA: "http://sky-hotel.co.uk/assets/swf/gamedata/productdata.txt",
-  FURNI_DATA: "http://sky-hotel.co.uk/assets/swf/gamedata/furnidata.xml",
+  PRODUCT_DATA: "http://sky-hotel.co.uk/arc/swf/gamedata/productdata.txt",
+  FURNI_DATA: "http://sky-hotel.co.uk/arc/swf/gamedata/furnidata.xml",
 };
 
 /*
