78293222a26fd0caa4e68a6738e8f5f87c9cc5a7
Ellis Goranov <me@ellis.pw>
2021-01-09 19:39:58 +0000

    setup api handler for online users page

--- skyapi/http/hotel/stats.js
+++ skyapi/http/hotel/stats.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import HttpMiddleware from "../middleware";
 import HotelUser from "../../database/models/users/user";
 
 export default class HttpStats {
@@ -6,6 +7,7 @@ export default class HttpStats {
         let router = Router();
 
         router.get("/total_online", this.total_online)
+        router.get("/online_users", HttpMiddleware.is_authenticated, this.online_users);
 
         return router;
     }
@@ -14,6 +16,19 @@ export default class HttpStats {
         new HotelUser().where({ online: '1' }).count()
         .then(result => {
             return res.status(200).json({ online: result });
+        })
+    }
+
+    online_users(req, res, next) {
+        new HotelUser().where({ online: '1' }).fetchAll({
+            columns: [
+                'id',
+                'username',
+                'look',
+                'online'
+            ]
+        }).then(result => {
+            return res.status(200).json(result.toJSON());
         })
     }
 }
